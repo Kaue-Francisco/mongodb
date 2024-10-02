f68c13a36d8e43aca03e49cd03581c613f5fc230
Kaue-Francisco <kauesantos.francisco@gmail.com>
2024-10-02 11:40:27 -0300

    refactor: Alterando nome do banco para a atividade do redis.

--- config/definitions.py
+++ config/definitions.py
@@ -3,6 +3,6 @@
 NAME_USER_DB = "root"
 PASSWD_USER_DB = "root"
 URL = f"mongodb+srv://{NAME_USER_DB}:{PASSWD_USER_DB}@fatec.eiqdu.mongodb.net/?retryWrites=true&w=majority&appName=FATEC"
-DB_NAME = "mercado_livre"
+DB_NAME = "mercado_livre_redis"
 
 ################################################################################
