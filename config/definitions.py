224abada1743f2d1289254a66b55e38a7866ec4a
Kaue-Francisco <kauesantos.francisco@gmail.com>
2024-11-08 20:08:47 -0300

    refactor: Removendo arquivos de configuração da atividade passada.

--- config/definitions.py
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Definitions
-NAME_USER_DB = "root"
-PASSWD_USER_DB = "root"
-URL = f"mongodb+srv://{NAME_USER_DB}:{PASSWD_USER_DB}@fatec.eiqdu.mongodb.net/?retryWrites=true&w=majority&appName=FATEC"
-DB_NAME = "mercado_livre"
-
-################################################################################
